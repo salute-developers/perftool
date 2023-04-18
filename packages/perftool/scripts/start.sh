da2049524f2220db0ac8c31c8463382f5d61e085
temariq <akhdrv@gmail.com>
2023-04-19 00:55:32 +0300

    test: fix integration

--- packages/perftool/scripts/start.sh
+++ packages/perftool/scripts/start.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env sh
 
-set -e
+set -ex
 
-PROJECT_DIR="$(dirname "$0")/.."
-OPTS="--experimental-specifier-resolution=node"
+case "$(uname -s)" in
+  Darwin*)
+    TRAVERSED_LINK=$(readlink $0 || true)
+
+    if [ -z "${TRAVERSED_LINK}" ]; then
+      TRAVERSED_LINK="start.sh"
+    fi
+
+    SCRIPT_PATH="$(dirname "$0")/$TRAVERSED_LINK"
+    ;;
+  *)          SCRIPT_PATH="$(readlink -f "$0")";;
+esac
+
+PROJECT_DIR="$(dirname "$SCRIPT_PATH")/.."
+TS_NODE_ESM_PATH=$(cd "$PROJECT_DIR" && node -e "console.log(require.resolve('ts-node/esm'))")
+OPTS="--loader $TS_NODE_ESM_PATH --experimental-specifier-resolution=node"
 
 if [ -z "$PERFTOOL_DEBUG" ]; then
   OPTS="$OPTS --no-warnings"
   set -x
 fi
 
-NODE_OPTIONS=$OPTS "$PROJECT_DIR/node_modules/.bin/ts-node-esm" --project "$PROJECT_DIR/tsconfig.json" --files "$PROJECT_DIR/src/index.ts" "$@"
+NODE_OPTIONS=$OPTS TS_NODE_PROJECT="$PROJECT_DIR/tsconfig.json" TS_NODE_FILES=true node "$PROJECT_DIR/src/index.ts" "$@"
